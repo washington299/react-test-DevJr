ddca5a8638eba1a736f98279655fbff7ba4db6d5
Washington Campos <washingtoncampos9@gmail.com>
2020-03-17 17:58:02 -0300

    Updates

---
+++ precache-manifest.25e038fd755ae12739b1d315358943be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afeeea7e87e7b3ec3aa7bc047d46185",
+    "url": "/react-test-DevJr/index.html"
+  },
+  {
+    "revision": "5189267cf6857618d5de",
+    "url": "/react-test-DevJr/static/js/2.f1c55b85.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/react-test-DevJr/static/js/2.f1c55b85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a832eec408106592d9b4",
+    "url": "/react-test-DevJr/static/js/main.71ca0109.chunk.js"
+  },
+  {
+    "revision": "fc3a4617c099f7518868",
+    "url": "/react-test-DevJr/static/js/runtime-main.e93034d9.js"
+  }
+]);
