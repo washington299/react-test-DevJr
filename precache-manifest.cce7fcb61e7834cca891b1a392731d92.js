2df6ada232a6bf5976473f5284e334af9a85c3f0
Washington Campos <washingtoncampos9@gmail.com>
2020-03-19 21:15:52 -0300

    Updates

---
+++ precache-manifest.cce7fcb61e7834cca891b1a392731d92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965b3724a525d3099b6af65aceb19822",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5224ae859ca1e2531778",
+    "url": "/static/js/2.7a57d92f.chunk.js"
+  },
+  {
+    "revision": "e2eae84c7a1d31e04c786e0eb21cdeb1",
+    "url": "/static/js/2.7a57d92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b695d8147b028835a8f",
+    "url": "/static/js/main.344f7a01.chunk.js"
+  },
+  {
+    "revision": "426f082e5e6c94ddfc14",
+    "url": "/static/js/runtime-main.ef7c0d79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-test-DevJr/precache-manifest.25e038fd755ae12739b1d315358943be.js"
+  "/precache-manifest.cce7fcb61e7834cca891b1a392731d92.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-test-DevJr/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
